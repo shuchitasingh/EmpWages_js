5dcd28c347236bb6fd40a9f8bdfa5aebfa525de0
shuchitasingh <singhshuchita5@gmail.com>
2021-05-26 18:18:45 +0530

    Added:emp wages no of wage and no of hr per month is reached

--- TotalWagesUnderGivenCondition.js
+++ TotalWagesUnderGivenCondition.js
@@ -4,6 +4,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck) {
     switch(empCheck){
@@ -18,12 +19,15 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
-
+ 
 let totalEmpHrs = 0;
-for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+let totalWorkingDays = 0;
+while((totalEmpHrs<=MAX_HRS_IN_MONTH)&&(totalWorkingDays<NUM_OF_WORKING_DAYS)){
     let empCheck = Math.floor(Math.random()*10)%3;
     totalEmpHrs += getWorkingHours(empCheck);
+    totalWorkingDays++;
 }
 
 let empWage = totalEmpHrs*WAGE_PER_HOUR;
+console.log("Total working days: "+totalWorkingDays);
 console.log("Total emp hours = "+totalEmpHrs+" Total employee wage = "+empWage);
