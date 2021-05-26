d1cfe6440a491d9d137ed7eb6099593131dc498a
shuchitasingh <singhshuchita5@gmail.com>
2021-05-26 17:44:57 +0530

    Added:wages for month

--- WagesForMonth.js
+++ WagesForMonth.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
     switch(empCheck){
@@ -18,7 +19,11 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empCheck = Math.floor(Math.random()*10)%3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Emp hours = "+empHrs+" Employee wage = "+empWage);
+let totalEmpHrs = 0;
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck = Math.floor(Math.random()*10)%3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+
+let empWage = totalEmpHrs*WAGE_PER_HOUR;
+console.log("Total emp hours = "+totalEmpHrs+" Total employee wage = "+empWage);
