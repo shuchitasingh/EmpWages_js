a2be73cb38b4b66dbc882a51d2d37bd58616ec36
shuchitasingh <singhshuchita5@gmail.com>
2021-05-26 17:17:37 +0530

    Added:employee wages part time or full time

--- EmpWage.js
+++
@@ -1,9 +0,0 @@
-const IS_ABSENT = 0;
-
-let empCheck = Math.floor(Math.random()*10)%2;
-if(empCheck == IS_ABSENT){
-    console.log("Employee is absent");
-}
-else {
-    console.log("Employee is present");
-}
